4954ea83713f53a69b78d02335cabc57f21bc9e7
brahimcode604 <b.elbahloul6370@uca.ac.ma>
2026-04-09 10:30:00 +0100

    feat: add module manifest with basic metadata

---
+++ bakery_management/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bakery Management',
+    'version': '17.0.0.1.0',
+    'category': 'Industries',
+    'summary': 'Gestion de patisserie',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
