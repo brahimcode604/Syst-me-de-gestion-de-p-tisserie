093682760370a79969e1db952f5c26380e3ca1b7
brahimcode604 <b.elbahloul6370@uca.ac.ma>
2026-04-12 10:30:00 +0100

    refactor(models): finalize order model - clean up TODO markers

--- bakery_management/models/bakery_order.py
+++ bakery_management/models/bakery_order.py
@@ -1,4 +1,3 @@
-# TODO: ajouter tests unitaires pour le workflow de commande
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError, UserError
